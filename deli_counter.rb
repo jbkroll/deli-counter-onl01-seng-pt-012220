a0307f36405b1398213f3c53ee4e46cb1a46a77c
Jennifer Kroll <jenniferbantakroll@gmail.com>
2020-02-02 21:38:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,13 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
+    current_sentence = "The line is currently:"
+    counter = 1
     katz_deli.each do |p|
-      binding.pry
+      current_sentence += " #{counter}. #{p}"
+      counter += 1
     end
+    current_sentence
   end
 end
 
