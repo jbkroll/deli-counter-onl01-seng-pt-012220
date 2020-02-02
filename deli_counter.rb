db6b68d6016e24b24b6313b80a56f37b341c2e87
Jennifer Kroll <jenniferbantakroll@gmail.com>
2020-02-02 21:33:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,9 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-    katz_deli.each do |n|
+    katz_deli.each do |p|
       binding.pry
+    end
   end
 end
 
