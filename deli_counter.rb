8fc7700271046c1f14337528ffa57cc99c68ca40
Jennifer Kroll <jenniferbantakroll@gmail.com>
2020-02-02 22:04:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli.push(name)
+  katz_deli << name
     puts "Welcome, #{name}. You are number #{katz.deli.length} in line."
 end
 
