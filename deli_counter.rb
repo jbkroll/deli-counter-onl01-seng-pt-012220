ed5f71a1b0e8af6a1a03b69db9db4c5c280fa5ec
Jennifer Kroll <jenniferbantakroll@gmail.com>
2020-02-02 21:17:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
-
+def line(katz_deli)
+  end
 
 
 1. Build the `line` method that shows everyone their current place in the line. If there is nobody in line, it should say `"The line is currently empty."`.
